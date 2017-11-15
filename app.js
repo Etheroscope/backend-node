65a9aa4a21f545366822d121caa505443cad7b12
Kian Mayne <git@kianmayne.co.uk>
2017-11-15 16:28:22 +0000

    Enable cross origin requests

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const ContractAPI = require('./api');
 const app = express();
 app.use(logger('dev'));
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/contracts/:address', function (req, res, next) {
   ContractAPI.getContractMetadata(req.params.address)
     .then(contract => res.json(contract))
